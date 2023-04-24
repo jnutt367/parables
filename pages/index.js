0ea60d9eb5d2c374611df6a503dbb29667417b4f
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-04-24 14:44:41 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -244,11 +244,11 @@ Parable of The Net &darr;</h2>
 
 <div className={styles.card}>
    <h2 className={styles.chapter}> Parable of The Lost Sheep &darr;</h2>
- <p>
-( MATTHEW  18:10-14 )
+ <p>( MATTHEW  18:10-14 )
 “See that you do not despise one of these little ones. For I tell you that their angels in heaven always see the face of my Father in heaven.
 
-“What do you think? If a man owns a hundred sheep, and one of them wanders away, will he not leave the ninety-nine on the hills and go to look for the one that wandered off? And if he finds it, truly I tell you, he is happier about that one sheep than about the ninety-nine that did not wander off. In the same way your Father in heaven is not willing that any of these little ones should perish.  </p>
+“What do you think? If a man owns a hundred sheep, and one of them wanders away, will he not leave the ninety-nine on the hills and go to look for the one that wandered off? And if he finds it, truly I tell you, he is happier about that one sheep than about the ninety-nine that did not wander off. In the same way your Father in heaven is not willing that any of these little ones should perish. 
+  </p>
   </div>
 
 
