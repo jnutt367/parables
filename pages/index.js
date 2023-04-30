adf03b6f34a2f3f6975cc807f51381af9c1d9376
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-04-30 13:32:54 -0400

    Update index.js
    <iframe width="100%" height="315" src="https://www.youtube.com/embed/zRC8COfdzTY" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
                               />
            </div>
 <p className={styles.parables}>Through speaking in parables, Jesus grants understanding to those who are seeking after Him – revealing truth to those who are willing to listen and thoughtfully consider what He has to say. Conversely, those whose hearts are hardened against Him have the truth hidden from them.</p>
-  <iframe width="100%" height="275" src="https://www.youtube.com/embed/nRU42q5xNWE" title="YouTube video player" frameborder="2" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+ <iframe width="100%" height="315" src="https://www.youtube.com/embed/zRC8COfdzTY" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                    
 
 
