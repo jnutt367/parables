cfbe6fe26c803f929e9cda4f4a718ec094c00a26
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-05-11 19:41:47 -0400

    Update index.js
      <div className={styles.card}>
                                            <h2 className={styles.chapter}>

--- pages/index.js
+++ pages/index.js
@@ -658,6 +658,28 @@ He said to them, “Do you bring in a lamp to put it under a bowl or a bed? Inst
               “‘My son,’ the father said, ‘you are always with me, and everything I have is yours. But we had to celebrate and be glad, because this brother of yours was dead and is alive again; he was lost and is found.’”
                  </p>
               </div>
+                                <div className={styles.card}>
+                                        <h2 className={styles.chapter}>Parable of The Rich Man and Lazarus</h2> 
+                                          <p>( LUKE 16:19-31 )
+                            “There was a rich man who was dressed in purple and fine linen and lived in luxury every day. At his gate was laid a beggar named Lazarus, covered with sores and longing to eat what fell from the rich man’s table. Even the dogs came and licked his sores.
+
+                            “The time came when the beggar died and the angels carried him to Abraham’s side. The rich man also died and was buried. In Hades, where he was in torment, he looked up and saw Abraham far away, with Lazarus by his side. So he called to him, ‘Father Abraham, have pity on me and send Lazarus to dip the tip of his finger in water and cool my tongue, because I am in agony in this fire.’
+
+                            “But Abraham replied, ‘Son, remember that in your lifetime you received your good things, while Lazarus received bad things, but now he is comforted here and you are in agony. And besides all this, between us and you a great chasm has been set in place, so that those who want to go from here to you cannot, nor can anyone cross over from there to us.’
+
+                            “He answered, ‘Then I beg you, father, send Lazarus to my family, for I have five brothers. Let him warn them, so that they will not also come to this place of torment.’
+
+                            “Abraham replied, ‘They have Moses and the Prophets; let them listen to them.’
+
+                            “‘No, father Abraham,’ he said, ‘but if someone from the dead goes to them, they will repent.’
+
+                            “He said to him, ‘If they do not listen to Moses and the Prophets, they will not be convinced even if someone rises from the dead.’”
+                                  </p>
+                                </div>
+
+
+
+
 
 
         </div>
