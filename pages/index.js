a18144702a308c9168c130f40cba95a692a7736c
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-12-14 16:46:06 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
        PARABLES
         </h1>
          <div className={styles.iframe1}>
-      <iframe width="100%" height="300" src="https://www.youtube.com/embed/N2VMmhClP-E?si=ZfUIUf9Ww5kStSuF?autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="100%" height="300" src="https://www.youtube.com/embed/N2VMmhClP-E?si=ZfUIUf9Ww5kStSuF&autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
            </div>
 <p className={styles.parables}> Jesus reveals "knowledge of the secrets of the kingdom of heaven" to those with ears to hear</p>
             <div className={styles.nav}>               
